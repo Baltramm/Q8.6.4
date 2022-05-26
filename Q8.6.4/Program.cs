0d26beac7421de56cf1c2ecc8812237f110e7d9c
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-05-26 19:58:58 +0600

    V1.1

--- Q8.6.4/Program.cs
+++ Q8.6.4/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.IO;
 
 namespace Q8._6._4
 {
     class Program
     {
+        const string desktop = "C://Users/Администратор/Desktop/";
+        const string path = "C://Users/Администратор/Desktop/Students.dat";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (!Directory.Exists(desktop))
+            {
+                try
+                {
+                   var directory = Directory.CreateDirectory(desktop); 
+                   
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+
+        static void ReadValues()
+        {
+            string StringValue;
+
+
+            if (File.Exists(path))
+            {
+                // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
+                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    // Применяем специализированные методы Read для считывания соответствующего типа данных.
+                    StringValue = reader.ReadString();
+                }
+
+                Console.WriteLine("Из файла считано:");
+                Console.WriteLine(StringValue);
+
+            }
         }
     }
 }
