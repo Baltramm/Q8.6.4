0c4d45a72b13b82aba93cfbeb49ff47199107058
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-05-26 21:32:19 +0600

    V1.3

--- Q8.6.4/Program.cs
+++ Q8.6.4/Program.cs
@@ -8,6 +8,7 @@ namespace Q8._6._4
         const string desktop = "C://Users/Администратор/Desktop/Students";
         const string path = "C://Users/Администратор/Desktop/Student.dat";
 
+
         static void Main(string[] args)
         {
             if (!File.Exists(desktop))
@@ -22,58 +23,51 @@ namespace Q8._6._4
                     Console.WriteLine(ex.Message);
                 }
             }
-            
-            var student1 = new Student();
-            student1.Name = "Олег";
-            student1.Group = "ИС-101";
-            student1.DateOfBirth = Convert.ToDateTime("01.01.2000");
-            var student2 = new Student();
-            student2.Name = "Игорь";
-            student2.Group = "ИС-102";
-            student2.DateOfBirth = Convert.ToDateTime("02.02.2001");
-            var student3 = new Student();
-            student3.Name = "Иван";
-            student3.Group = "ИС-103";
-            student3.DateOfBirth = Convert.ToDateTime("03.03.2002");
-            
+            WriteValues();
+            ReadValues();
         }
 
 
         static void ReadValues()
         {
             string StringValue;
-
-
             if (File.Exists(path))
             {
         
                 using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
                 {
-                
-                    StringValue = reader.ReadString();
+                    StringValue = reader.ReadString();     
                 }
-
                 Console.WriteLine("Из файла считано:");
                 Console.WriteLine(StringValue);
-
             }
         }
         static void WriteValues()
         {
-     
+            var student1 = new Student("Олег", "ИС-101", Convert.ToDateTime("01.01.2000"));
+            var student2 = new Student("Игорь", "ИС-102", Convert.ToDateTime("02.02.2001"));
+            var student3 = new Student("Иван", "ИС-103", Convert.ToDateTime("03.03.2002"));
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
             {
-              
-               
-                writer.Write($"");
+                writer.Write($"Имя:{student1.Name},Группа:{student1.Group},Дата рождения:{student1.DateOfBirth}\n");
+                writer.Write($"Имя:{student2.Name},Группа:{student2.Group},Дата рождения:{student2.DateOfBirth}\n");
+                writer.Write($"Имя:{student3.Name},Группа:{student3.Group},Дата рождения:{student3.DateOfBirth}\n");
             }
         }
 
     }
+  
     class Student
     {
         public string Name { get; set; }
         public string Group { get; set; }
         public DateTime DateOfBirth { get; set; }
+
+        public Student (string name, string group, DateTime dateOfBirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateOfBirth;
+        }
     }
 }
