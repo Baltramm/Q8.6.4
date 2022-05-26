940a960e1e8ecb901075791c4bc4503eaa2a69d6
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-05-26 23:07:55 +0600

    Final

--- Q8.6.4/Program.cs
+++ Q8.6.4/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-namespace Q8._6._4
+namespace FinalTask
 {
     class Program
     {
         const string desktop = "C://Users/Администратор/Desktop/Students";
-        const string path = "C://Users/Администратор/Desktop/Student.dat";
+        const string path = "C://Users/Администратор/Desktop/Students(2).dat";
 
 
         static void Main(string[] args)
@@ -23,51 +24,42 @@ namespace Q8._6._4
                     Console.WriteLine(ex.Message);
                 }
             }
-            WriteValues();
+           
             ReadValues();
         }
 
 
         static void ReadValues()
         {
-            string StringValue;
+            
             if (File.Exists(path))
             {
-        
-                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+                var formatter = new BinaryFormatter();
+                Student[] students;
+
+                using (var stream = new FileStream(path, FileMode.Open))
                 {
-                    StringValue = reader.ReadString();     
+                    students = formatter.Deserialize(stream) as Student[];
+                    Console.WriteLine("Из файла считано:");
+                    Console.WriteLine(students[0].Group);
                 }
-                Console.WriteLine("Из файла считано:");
-                Console.WriteLine(StringValue);
-            }
-        }
-        static void WriteValues()
-        {
-            var student1 = new Student("Олег", "ИС-101", Convert.ToDateTime("01.01.2000"));
-            var student2 = new Student("Игорь", "ИС-102", Convert.ToDateTime("02.02.2001"));
-            var student3 = new Student("Иван", "ИС-103", Convert.ToDateTime("03.03.2002"));
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
-            {
-                writer.Write($"Имя:{student1.Name},Группа:{student1.Group},Дата рождения:{student1.DateOfBirth}\n");
-                writer.Write($"Имя:{student2.Name},Группа:{student2.Group},Дата рождения:{student2.DateOfBirth}\n");
-                writer.Write($"Имя:{student3.Name},Группа:{student3.Group},Дата рождения:{student3.DateOfBirth}\n");
+                foreach (var St in students) 
+                {
+                    File.AppendAllText($"C://Users/Администратор/Desktop/Students/{St.Group}.txt", $"{St.Name},{St.DateOfBirth}\n");
+                }
             }
         }
 
+
     }
-  
+
+    [Serializable]
     class Student
     {
         public string Name { get; set; }
         public string Group { get; set; }
         public DateTime DateOfBirth { get; set; }
 
-        public Student (string name, string group, DateTime dateOfBirth)
-        {
-            Name = name;
-            Group = group;
-            DateOfBirth = dateOfBirth;
-        }
+
     }
 }
